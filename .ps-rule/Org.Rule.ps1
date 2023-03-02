138c20b9601221226b02b560a4da04c59818f042
Bernie White <bewhite@microsoft.com>
2023-03-02 23:25:52 +1000

    Updates for demo

--- .ps-rule/Org.Rule.ps1
+++ .ps-rule/Org.Rule.ps1
@@ -3,3 +3,35 @@
 
 # Note:
 # This script demonstrates using PowerShell-based rules.
+
+#region Conventions
+
+# Synopsis: Imports in Bicep module paths for analysis.
+Export-PSRuleConvention 'Org.BicepModule' -Initialize {
+    $modules = @(Get-ChildItem -Path 'bicep/modules/' -Include 'main.bicep' -Recurse -File | ForEach-Object {
+        $version = $_.Directory.Name
+        $name = $_.Directory.Parent.Name
+        [PSCustomObject]@{
+            Name = $name
+            Version = $version
+            Path = $_.Directory.FullName
+        }
+    })
+    $PSRule.ImportWithType('Azure.Bicep.ModuleInfo', $modules);
+}
+
+#endregion Conventions
+
+#region Custom Rules
+
+# Synopsis: All modules must have a corresponding tests file.
+Rule 'Org.RequireTests' -Type 'Azure.Bicep.ModuleInfo' {
+    $Assert.FilePath($TargetObject, 'Path', @('.tests/main.tests.bicep'))
+}
+
+# Synopsis: All modules must have a corresponding README file.
+Rule 'Org.RequireReadme' -Type 'Azure.Bicep.ModuleInfo' {
+    $Assert.FilePath($TargetObject, 'Path', @('README.md'))
+}
+
+#enregion Custom Rules
